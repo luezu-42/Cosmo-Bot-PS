51b5d18fe644f3e43f1ded19b57f6b807903ef5a
luezu-42 <44908786+luezu-42@users.noreply.github.com>
2020-02-28 19:00:23 -0300

    Teste 3 semi-feito

--- Teste-3/index.js
+++ Teste-3/index.js
@@ -1,7 +1,23 @@
 const button = document.getElementById('button');
 const resultado = document.getElementById('resultado');
-
+const mat = [];
 
 button.addEventListener('click', ()=>{
+    const numeros = document.getElementById('numeros').value;
+    mat.push(numeros[0]);
 
+            
+    for(let z = 0;z < mat.length; z++){
+                
+        for(let ordem = mat[z].length - 1;ordem > 0;ordem--){                    
+            for(let i = 0;i < ordem;i++){                        
+                if(mat[z][i] > mat[z][i+1]){                            
+                    [mat[z][i], mat[z][i+1]] = [mat[z][i+1], mat[z][i]]
+                        
+                        }
+        	        }
+        	    }
+            }
+            
+    resultado.innerHTML = mat; 
 });
