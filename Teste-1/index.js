528e05ddbdf4973c441851b93322fcb6ac424329
luezu-42 <44908786+luezu-42@users.noreply.github.com>
2020-02-29 14:07:53 -0300

    Correção do código: Teste 1

--- Teste-1/index.js
+++ Teste-1/index.js
@@ -1,5 +1,6 @@
 const resultado = document.getElementById('resultado');
 const botao = document.getElementById('botao');
+const mat = [];
 
 botao.addEventListener('click', ()=>{
     const palavras = document.getElementById('palavras').value;
@@ -9,4 +10,49 @@ botao.addEventListener('click', ()=>{
 
     resultado.innerHTML = reverse;
     
+});
+
+
+
+//    Correção do código
+//          
+//          \|
+//         _\|__
+//        \    |
+//         \  |
+//          \|
+//
+//botao.addEventListener('click', ()=>{
+//    mat.length = [];
+//    let z = 0;
+//    let i = 0;
+//
+//    const palavras = document.getElementById('palavras').value;
+//    mat.push(palavras.split(' ').reverse());
+//
+//    let texto = '';
+//
+//    resultado.innerHTML = '';
+//
+//    for(i = 0;i < mat.length; i++){
+//        for(z = 0;z < mat[i].length;z++){
+//            
+//            if(mat[i][0] == 0){
+//
+//            texto = `${mat[i][z]}` + ` `;
+//            
+//            }else{
+//                
+//            texto += ` ` + `${mat[i][z]}`;
+//
+//            }
+//    }
+//    
+//    
+//}
+//resultado.innerHTML = ` ${texto} `;
+//});
+
+palavras.addEventListener('focus', ()=>{
+    document.getElementById('palavras').value = '';
 });
